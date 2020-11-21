b69e6ea2d17ae7aa7e8dbf66736feddaffb4ec66
Aaron Rory Newbold <aaron.rory.newbold@gmail.com>
2020-11-21 13:48:20 -0500

    Update activation_mailer.rb

--- app/mailers/activation_mailer.rb
+++ app/mailers/activation_mailer.rb
@@ -4,9 +4,9 @@ class ActivationMailer < ApplicationMailer
   default from: 'forumCMS@notifications.com'
 
   def welcome_email
-    site = 'arn-forum-cms.herokuapp.com'
+    site = 'arn-forum-api.herokuapp.com'
     @user = params[:user]
-    @url  = "http://#{site}/registrations/#{@user.id}/activate_account/#{@user.activation_key}"
+    @url  = "https://#{site}/registrations/#{@user.id}/activate_account/#{@user.activation_key}"
     mail(to: @user.email, subject: 'Welcome to the React.js Forum-CMS Demo')
   end
 end
