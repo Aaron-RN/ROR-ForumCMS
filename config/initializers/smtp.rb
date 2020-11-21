1d4865e78a67a44910c532aa59b7437b45fc43cf
Aaron Rory Newbold <aaron.rory.newbold@gmail.com>
2020-11-21 11:37:49 -0500

    Update smtp.rb

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -2,7 +2,7 @@ ActionMailer::Base.smtp_settings = {
   address: 'smtp.sendgrid.net',
   port: 587,
   domain: 'arn-forum-api.herokuapp.com',
-  user_name:'apikey',
-  password:ENV['SENDGRID_API_KEY']
+  user_name: 'apikey',
+  password: ENV['SENDGRID_API_KEY'],
   authentication: :plain
 }
