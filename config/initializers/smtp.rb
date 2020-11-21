a0393f08dd740d4459aefc0162c846a5256600ca
Aaron Rory Newbold <aaron.rory.newbold@gmail.com>
2020-11-21 11:34:03 -0500

    Update smtp.rb
    Swapped out username and password for Sendgrid API key

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -2,8 +2,7 @@ ActionMailer::Base.smtp_settings = {
   address: 'smtp.sendgrid.net',
   port: 587,
   domain: 'arn-forum-api.herokuapp.com',
-  user_name: ENV['SENDGRID_USERNAME'],
-  password: ENV['SENDGRID_PASSWORD'],
-  authentication: :plain,
-  enable_starttls_auto: true
+  user_name:'apikey',
+  password:ENV['SENDGRID_API_KEY']
+  authentication: :plain
 }
