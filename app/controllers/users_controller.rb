4189669ab123f9bf40cdadfe14a4c0631df54d26
Aaron Rory Newbold <aaron.rory.newbold@gmail.com>
2020-12-13 17:47:18 -0500

    bug fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -60,8 +60,11 @@ class UsersController < ApplicationController
     # return unless comms.is_a?(Array)
 
     comms_i = comms.map(&:to_i)
+    puts "#{comms_i}"
     ban_date = DateTime.new(comms_i[0], comms_i[1], comms_i[2], comms_i[3], comms_i[4]);
+    puts ban_date
     user.update_attribute(attr, ban_date)
+    puts user.update_attribute(attr, ban_date)
   end
 
   # Returns a hash object of a user with their profile_image included
