8e39fe478f1a3a5fb5e60fd0603089cdec063fe4
Aaron Rory Newbold <aaron.rory.newbold@gmail.com>
2021-01-07 17:27:18 -0500

    added password_reset method

--- app/mailers/activation_mailer.rb
+++ app/mailers/activation_mailer.rb
@@ -10,4 +10,12 @@ class ActivationMailer < ApplicationMailer
     # @url = "https://#{site}/registrations/#{@user.id}/activate_account"
     mail(to: @user.email, subject: 'Welcome to the React.js Forum-CMS Demo')
   end
+  
+  def password_reset_email
+    # site = 'arn-forum-api.herokuapp.com'
+    @user = params[:user]
+    # @activation_key = @user.activation_key
+    # @url = "https://#{site}/registrations/#{@user.id}/activate_account"
+    mail(to: @user.email, subject: 'Forgot your password?')
+  end
 end
