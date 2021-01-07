5da888646909eccda180782138195cb60a88d47b
Aaron Rory Newbold <aaron.rory.newbold@gmail.com>
2021-01-07 18:52:05 -0500

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -15,7 +15,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins url
 
-    resource '*', headers: :any, methods: allowed_methods, credentials: false
     resource '/activate_account', headers: :any, methods: %i[get]
     resource '/users/update_image', headers: :any, methods: %i[patch]
     resource '/users/set_admin_level', headers: :any, methods: %i[patch]
@@ -25,7 +24,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 #     resource '/logged_in', headers: :any, methods: %i[get]
     resource '/log_in', headers: :any, methods: %i[post]
     resource '/logout', headers: :any, methods: %i[patch]
-    resource '/forums', headers: :any, methods: %i[get post]
+    resource '/forums', headers: :any, methods: %i[post]
+    resource '/forums*', headers: :any, methods: %i[get]
     resource '/forums/*', headers: :any, methods: %i[patch delete]
     resource '/subforums', headers: :any, methods: %i[post]
     resource '/subforums/*', headers: :any, methods: %i[patch delete]
@@ -35,5 +35,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource '/posts/lock_post', headers: :any, methods: %i[patch]
     resource '/comments', headers: :any, methods: %i[get post]
     resource '/comments/*', headers: :any, methods: %i[get patch delete]
+    resource '*', headers: :any, methods: allowed_methods, credentials: false
   end
 end
