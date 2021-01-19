5ee1ea786b406484fb2d03ed398b98074dbf2d35
Aaron-RN <aaron_newbold@hotmail.com>
2021-01-19 14:46:46 -0500

    fixed exceptions for posts and their optional subforum attr

--- app/models/post.rb
+++ app/models/post.rb
@@ -27,7 +27,7 @@ class Post < ApplicationRecord
       new_post['title'] = post.title.slice(0..30)
       new_post['body'] = post.body.slice(0..32)
       new_post['author'] = post.author.username
-      new_post['subforum'] = post.subforum.name if subforum.present?
+      new_post['subforum'] = post.subforum.name if post.subforum.present?
       new_post['forum'] = post.forum.name
       new_post['admin_only'] = post.forum.admin_only
       new_post['admin_only_view'] = post.forum.admin_only_view
